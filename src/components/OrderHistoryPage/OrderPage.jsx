434ce1214bb1bb0f33818f998f6c376d4ae38c60
Andrey2191 <oxido9055@gmail.com>
2022-01-24 20:33:35 +0300

    fix order

--- src/components/OrderHistoryPage/OrderPage.jsx
+++ src/components/OrderHistoryPage/OrderPage.jsx
@@ -5,6 +5,7 @@ import { fetchOrder } from "../../redux/slices/orderSlice";
 import { useEffect } from "react";
 import OrderList from "./OrderList";
 import classNames from "classnames";
+import { objectOf } from "prop-types";
 
 export default function OrderPage() {
   const orders = useSelector((state) => state.orders.orders);
@@ -15,26 +16,30 @@ export default function OrderPage() {
   return (
     <div className="history--page">
       <h1>Your order</h1>
-      <div className={classNames("orderCard")}>
-        {orders.map((order) => {
-          <div>{order.email}</div>;
-          console.log(order.email);
-          console.log(order.address);
-          console.log(order.name);
-          console.log(order.telephone);
-          console.log(typeof orders);
-        })}
-      </div>
+      {/* <div className={classNames("orderCard")}> */}
       {/* {orders.map((order) => {
-        <span>{order.email}</span>;
-        console.log(order.email);
+          return <div>{order}</div>;
+        })}
+      </div> */}
+      {orders.map((order) => {
+        // <span>{order.email}</span>;
+
+        return (
+          <div>
+            <span>{order.email}</span>
+            <span>{order.name}</span>
+            <span>{order.telephone}</span>
+            <span>{order.address}</span>
+          </div>
+        );
         // <OrderList
+        //   key={order.id}
         //   userName={order.name}
         //   userEmail={order.email}
         //   userAddress={order.address}
         //   userTelephone={order.telephone}
-        // />;
-      })} */}
+        // />
+      })}
     </div>
   );
 }
